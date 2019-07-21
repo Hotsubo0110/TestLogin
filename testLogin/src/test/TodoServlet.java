553fb7ad73a1449c6196eac7223caf5c3327cc22
Hotsubo <hiroyan0110@gmail.com>
2019-07-21 15:07:36 +0900

    ログイン画面作成 servlet 500エラー

---
+++ testLogin/src/test/TodoServlet.java
@@ -0,0 +1,59 @@
+package test;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TodoServlet
+ */
+@WebServlet("/TodoServlet")
+public class TodoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private static final String JSP_BASE = "/WEB-INF/jsp/";
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TodoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String action = request.getParameter("action");
+
+		String forward = null;
+
+		if ("login".equals(action))
+		{
+			forward = JSP_BASE + "login.jsp";
+		}
+		else
+		{
+			throw new ServletException("不正なリクエストです");
+		}
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher(forward);
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
